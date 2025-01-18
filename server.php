37c438eb1291dd319794739fc1c7b1240ee529d9
Lewins Mureithi <161879514+King-luiz@users.noreply.github.com>
2025-01-18 19:37:25 +0300

    Update server.php

--- server.php
+++ server.php
@@ -17,7 +17,7 @@ if (isset($_POST['reg_user'])) {
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  // form validation: ensure that the form is correctly filled ...
+  // form validation: ensures that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
@@ -32,7 +32,7 @@ if (isset($_POST['reg_user'])) {
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
-  if ($user) { // if user exists
+  if ($user) { // if user exists gives out a message
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
     }
