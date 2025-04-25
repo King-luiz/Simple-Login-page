95254b6fec5106d07196578aaef6359060c35fc2
Lewins Mureithi <161879514+King-luiz@users.noreply.github.com>
2025-04-25 15:43:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -28,10 +28,7 @@
   	<?php if (isset($_SESSION['success'])) : ?>
       <div class="error success" >
       	<h3>
-          <?php 
-          	echo $_SESSION['success']; 
-          	unset($_SESSION['success']);
-          ?>
+          
       	</h3>
       </div>
   	<?php endif ?>
